b0350fb48ca8f1c48f1b040ec5276778d47d29ab
eranda-palliyaguru <erandasampath2000@gmail.com>
2023-12-16 15:12:56 +0530

    discount update

--- sales_save.php
+++ sales_save.php
@@ -2,11 +2,11 @@
 session_start();
 include('connect.php');
 date_default_timezone_set("Asia/Colombo"); 
-
+ 
 $a1 = $_REQUEST['name'];
 $f = $_REQUEST['qty'];
 
-$e = 0;
+$dis = $_REQUEST['dis'];
 
 $c = $_REQUEST['invoice'];
 $type_q = 0;
@@ -28,9 +28,9 @@ $result = $db->prepare("SELECT * FROM product WHERE product_id = '$a1' ");
 
 if($co=="pu"){$d=$cost;}
 
+$discount=($d/100)*$dis;
 
-
-$d=$d-$e;
+$d=$d-$discount;
 
 $profit=$d-$cost;
 $profit=$profit*$f;
@@ -44,7 +44,7 @@ $date=date("Y-m-d");
 // query
 $sql = "INSERT INTO sales_list (product_id,name,invoice_no,price,dic,qty,code,profit,type,date,amount,cost) VALUES (:a,:b,:c,:d,:e,:f,:g,:pro,:type,:date,:amount,:cost)";
 $ql = $db->prepare($sql);
-$ql->execute(array(':a'=>$a1,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$e,':f'=>$f,':g'=>$g,':pro'=>$profit,':type'=>$type,':date'=>$date,':amount'=>$amount,':cost'=>$cost));
+$ql->execute(array(':a'=>$a1,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$discount,':f'=>$f,':g'=>$g,':pro'=>$profit,':type'=>$type,':date'=>$date,':amount'=>$amount,':cost'=>$cost));
 
 if(isset($_REQUEST['end'])){
 	header("location: app/sales.php?id=$c");
